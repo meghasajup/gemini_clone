ea17ceda03ecd713d8f72e6c72375a46b13e9431
9400749917megha <meghasaju45@gmail.com>
2024-11-17 13:43:47 +0530

    remove dark mode

--- src/components/Sidebar/Sidebars.jsx
+++ src/components/Sidebar/Sidebars.jsx
@@ -38,7 +38,6 @@ const Sidebars = () => {
             </div>
 
             <div className="bottom">
-                {/* <DarkModeToggle /> */}
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt="" />
                     {extended ? <p>Help</p> : null}
